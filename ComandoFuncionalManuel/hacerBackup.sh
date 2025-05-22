45530d7bad52dc72a2012a469eab923607cc3f15
ManuelNey <mney.cabrera@correo.ucu.edu.uy>
2025-05-22 00:10:37 +0000

    Agrego mis comando

---
+++ ComandoFuncionalManuel/hacerBackup.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+nombre=""
+fecha=$(date +%y-%m-%d)
+hora=$(date +%H:%M)
+directorioOrigen=""
+directorioDestino=""
+explicacionVerbose=false
+comprimir=true
+
+while [[ $# -gt 0 ]]; do 
+	opcion="$1"
+	case "$opcion" in 
+		-o) 
+		   directorioOrigen="$2"
+		   shift 2
+		   ;;
+		-d)
+		   directorioDestino="$2"
+		   shift 2
+		   ;;
+		-n)
+		   nombre="$2"
+		   shift 2
+		   ;;
+		-v)
+		   explicacionVerbose=true 
+		   shift 
+		   ;;
+	        -nc)
+		   comprimir=false
+		   shift
+		   ;;
+		 *)	
+		   echo "Opcion no valida"
+	       	   exit 1
+		   ;;
+
+	esac
+done
+
+echo "termine el while"
+if [[ -z "$directorioOrigen" || -z "$directorioDestino" ]]; then 
+	echo "Los datos ingresados no son validos"
+	exit 1
+fi
+
+if [[ "$comprimir" == true ]]; then 
+	terminacion="tar.gz"
+	tipoTar="czf"
+else 
+	terminacion="tar"
+	tipoTar="cf"
+fi
+
+if [[ -n "$nombre" ]]; then
+	archivoGuardado="$directorioDestino/${nombre}.${terminacion}"
+else
+	archivoGuardado="$directorioDestino/backup-${fecha}_${hora}.${terminacion}"
+fi
+
+mkdir -p "$directorioDestino"
+
+if [[ "$explicacionVerbose" == true ]]; then 
+	echo "Guardando backup de  $directorioOrigen a $directorioDestino"
+fi
+tar $tipoTar "$archivoGuardado" -C "$(dirname "$directorioOrigen")" "$(basename "$directorioOrigen")"
+
+echo "llegue"
+if [[ "$explicacionVerbose" == true ]]; then
+	echo "El back se ha realizado con exito"
+fi
---
+++ ComandoFuncionalManuel/hacerBackupCronTab.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+directorioOrigen=""
+directorioDestino=""
+explicacionVerbose=false
+comprimir=true
+nombre=""
+
+hora="0"
+minuto="0"
+diaMes="*"
+mes="*"
+diaSemana="*"
+
+while [[ $# -gt 0 ]]; do 
+	opcion="$1"
+	case "$opcion" in 
+		-o) 
+		   directorioOrigen="$2"
+		   shift 2
+		   ;;
+		-d)
+		   directorioDestino="$2"
+		   shift 2
+		   ;;
+		-n)
+		   nombre="$2"
+		   shift 2
+		   ;;
+		-m)
+		   minuto="$2"
+		   shift 2
+		   ;;
+		-h)
+		   hora="$2"
+		   shift 2
+		   ;;
+		-dm)
+		   diaMes="$2"
+		   shift 2
+		   ;;
+		-nm)
+		   mes="$2"
+		   shift 2
+		   ;;
+		-ds)
+		   diaSemana="$2"
+		   shift 2
+		   ;;
+		-nc)
+		   comprimir=false
+		   shift
+		   ;;	
+	   	 -v)
+		   explicacionVerbose=true
+		   shift
+		   ;;
+		 *)	
+		   echo "Opcion no valida"
+	       	   exit 1
+		   ;;
+
+	esac
+
+done
+
+echo "termine el while"
+if [[ -z "$directorioOrigen" || -z "$directorioDestino" ]]; then 
+	echo "Los datos ingresados no son validos"
+	exit 1
+fi
+
+if [[ -z "$nombre" ]]; then 
+	echo "S necesita un nombre fijo para mantener el back up"
+	exit 1
+fi
+
+hacerBackup="/home/ManuelNey/TrabajoOS/hacerbackup.sh"
+
+comando="$hacerBackup -o '$directorioOrigen' -d '$directorioDestino' -n '$nombre'"
+
+tiempoCron="$minuto $hora $diaMes $mes $diaSemana"
+echo "$tiempoCron"
+if [[ "$comprimir" == false ]]; then 
+	comando+="-nc"
+fi
+
+if [[ "$explicacionVerbose" == true ]]; then 
+	comando+=" -v"
+fi
+
+linea="$tiempoCron $comando BackupDe:$nombre"
+crontab -l 2>/dev/null | grep -v "BackUp:$nombre" ; echo "$linea" | crontab -
+echo "Ya termine"
+
